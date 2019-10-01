2914b0aa32f0992117ec8b0ed6388d7c4a23e0b0
Zahreddine <zahreddine.atoui@issatso.u-sousse.tn>
2019-10-01 23:32:04 +0100

    fix  bugs

--- src/index.js
+++ src/index.js
@@ -15,10 +15,7 @@ const rootReducer = combineReducers({
   order: orderReducer,
   auth: authReducer
 });
-const composeEnhancers =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
